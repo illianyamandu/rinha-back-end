2c9f3cfc2fa1a9fb63813166e069f50ea436c81c
Illian Yamandú <yamandu.developer@gmail.com>
2024-01-23 07:56:38 -0300

    methods for axum routes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use axum::{
+    http::StatusCode,
+    response::IntoResponse,
     routing::{get, post},
     Router,
 };
@@ -7,12 +9,28 @@ use axum::{
 async fn main() {
     // build our application with a single route
     let app = Router::new()
-        .route("/pessoas", get(|| async { "Busca Pessoas" }))
-        .route("/pessoas/:id", get(|| async { "Pessoa por id" }))
-        .route("/contagem-pessoas", get(|| async { "Contar Pessoas" }))
-        .route("/pessoas", post(|| async { "Criar pessoas" }));
+        .route("/pessoas", get(search_people))
+        .route("/pessoas/:id", get(find_person))
+        .route("/contagem-pessoas", get(count_people))
+        .route("/pessoas", post(create_person));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn search_people() -> impl IntoResponse {
+    (StatusCode::OK, "Busca Pessoas")
+}
+
+async fn find_person() -> impl IntoResponse {
+    (StatusCode::OK, "Find")
+}
+
+async fn create_person() -> impl IntoResponse {
+    (StatusCode::OK, "Person")
+}
+
+async fn count_people() -> impl IntoResponse {
+    (StatusCode::OK, "Count")
 }
